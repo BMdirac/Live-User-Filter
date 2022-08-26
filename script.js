2142a9e77a336cb496b4233105938798c4d937eb
BMdirac <busarimoruf20@gmail.com>
2022-08-26 22:10:25 +0100

    new changes

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+// ********************** FINAL LEARNING PHASE *************************************************
+
 const result = document.getElementById('result')
 const filter = document.getElementById('filter')
 const listItems = [] // initialize an array (empty array where we will put the data that we fetch)
+
+filter.addEventListener('input', (e) => filterData(e.target.value))
 
 async function getData() {
     // its going to represent response from our fetch request
@@ -10,19 +14,80 @@ async function getData() {
     // res is going to be promise.
     const { results } = await res.json() // destructure
 
-    // Clear results
-    results.innerHTML = ''
+    // Clear result
+    result.innerHTML = ''
 
     // to loop through the data
     results.forEach(user => {
-        // to construct the users
-        
+        // to construct list item for each of the users
+        const li = document.createElement('li')
+
+        listItems.push(li)
+
+        li.innerHTML = `
+            <img src="${user.picture.large}" alt="${user.name.first}">
+            <div class="user-info">
+                <h4>${user.name.first} ${user.name.last}</h4>
+                <p>${user.location.city}, ${user.location.country}</p>
+            </div>
+        `
+        // to put the created list into the ul ( the result id in html)
+        result.appendChild(li)
     })
 }
 
 getData()
 
-// ********************** LEARNING PHASE 2 *************************************
+function filterData(searchTerm) {
+    listItems.forEach(item => {
+        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+            item.classList.remove('hide')
+        } else {
+            item.classList.add('hide')
+        }
+    })
+}
+
+// ********************** LEARNING PHASE 3 *************************************************
+
+// const result = document.getElementById('result')
+// const filter = document.getElementById('filter')
+// const listItems = [] // initialize an array (empty array where we will put the data that we fetch)
+
+// async function getData() {
+//     // its going to represent response from our fetch request
+//     const res = await fetch('https://randomuser.me/api/?results=50')
+
+//     // to get our data after fetch
+//     // res is going to be promise.
+//     const { results } = await res.json() // destructure
+
+//     // Clear result
+//     result.innerHTML = ''
+
+//     // to loop through the data
+//     results.forEach(user => {
+//         // to construct list item for each of the users
+//         const li = document.createElement('li')
+
+//         listItems.push(li)
+
+//         li.innerHTML = `
+//             <img src="${user.picture.large}" alt="${user.name.first}">
+//             <div class="user-info">
+//                 <h4>${user.name.first} ${user.name.last}</h4>
+//                 <p>${user.location.city}, ${user.location.country}</p>
+//             </div>
+//         `
+//         // to put the created list into the ul ( the result id in html)
+//         result.appendChild(li)
+//     })
+// }
+
+// getData()
+
+
+// ********************** LEARNING PHASE 2 *************************************************
 
 // const result = document.getElementById('result')
 // const filter = document.getElementById('filter')
